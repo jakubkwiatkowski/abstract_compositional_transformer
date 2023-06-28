ef6fc85ed9283dae59c471fa42140a89fd731110
Jakub Kwiatkowski <jkwiatkowski@man.poznan.pl>
2023-06-28 02:34:51 +0200

    feat: Add train scripts.

--- README.md
+++ README.md
@@ -1,29 +1,29 @@
-#  Abstract Compositional Transformer (ACT)
+# Abstract Compositional Transformer (ACT)
 
-This repository provides an implementation  Abstract Compositional Transformer (ACT).
+This repository provides an implementation Abstract Compositional Transformer (ACT).
 It was developed for research purposes by the Neurosymbolic Systems Lab at Poznan University of Technology.
 
-[Live demo](https://act-demo.herokuapp.com/)
+[Live demo](https://huggingface.co/spaces/jkwiatkowski/raven)
 
 ![example](images/example.png)
 ![example_2](images/example_2.png)
 
-
 # Installation
 
-You need pipenv to install the package.  To install the package, run the following command:
+You need pipenv to install the package. To install the package, run the following command:
 
 ```bash
 pipenv install
 ```
 
 # Usage
+
 1. Train the model
+
 ```bash
 python train.py
 ```
+
 2. Evaluate the model
-```bash
-python evaluate.py --model_path <path_to_model_weights>
-```
+3. 
 The weights are available in 
---
+++ train.py
@@ -0,0 +1,57 @@
+from dataclasses import asdict
+
+import tensorflow as tf
+
+tf.experimental.numpy.experimental_enable_numpy_behavior(prefer_float32=True)
+
+from huggingface_hub import from_pretrained_keras
+from datasets import load_dataset
+import numpy as np
+
+from raven_utils.models.transformer import get_rav_trans
+from raven_utils.params import TaskTokenizerParameters
+
+repo = "jkwiatkowski/raven"
+
+data = load_dataset(repo, split="val")
+properties = load_dataset(repo + "_properties", split="val")
+
+index = 0
+d = data[index:index + 1]['inputs']
+d = {
+    'inputs': np.asarray(d, dtype="uint8"),
+    'index': np.zeros(shape=(1, 1), dtype="uint8") + 8,
+    'target': np.zeros(shape=(1, 16, 113), dtype="int8"),
+}
+
+model = get_rav_trans(
+    d,
+    **asdict(TaskTokenizerParameters())
+)
+
+
+def generator():
+    for d in data:
+        d = {
+            'inputs': np.asarray(d['inputs'], dtype="uint8"),
+            'index': np.asarray(d['index'], dtype="uint8")[..., None],
+            'target': np.asarray(d['target'], dtype="uint8"),
+        }
+        yield d
+
+
+tf_data = tf.data.Dataset.from_generator(generator,
+                                         output_shapes=
+                                         {'inputs': (None, 16, 160, 160), 'index': (None, 1), 'target': (None, 16, 113)}
+                                         ,
+                                         output_types={
+                                             'inputs': "uint8",
+                                             'index': "uint8",
+                                             'target': "uint8",
+                                         },
+                                         )
+model.compile()
+model.fit(tf_data)
+model(data[0])
+
+print("End")
